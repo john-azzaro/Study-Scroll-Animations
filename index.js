020ee6003dea19f70f999fb77888d272bf3b6559
John Azzaro <jazzaro@temple.edu>
2019-08-14 21:34:16 -0700

    debugging

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ function scrollAppear() {                                                       
     let para = document.querySelector('.para');                                  /* Select the para element and store as "para" variable */
     let paraPosition =  para.getBoundingClientRect().top;                            /* get the distance of where this element is relative to the window  by getting "para" and the methos .getBoundingClientRect(angle), then access the top property. */
     let screenHeight = window.innerHeight / 1.3;                                      /* use this to make sure it works for different screen sizes */
-    console.log('paraPosition' + paraPosition);
+    console.log('paraPosition ' + paraPosition);
     console.log(screenHeight);
 
     if(paraPosition < screenHeight) {                                           /* if the position of the para element in less than the screen position, ....*/
-        para.classList.add('.para-appear');
+        $(".para").addClass(".para-appear");
         console.log('it worked')
     }
 }
