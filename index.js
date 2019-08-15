3b1cd006d3fe7863b16835e789739cb90a697d1b
John Azzaro <jazzaro@temple.edu>
2019-08-14 21:04:27 -0700

    add javascript

---
+++ index.js
@@ -0,0 +1,4 @@
+function scrollAppear() {                                                        /* This function will control the reveal of the element on scroll.*/
+    let para = document.querySelector('.para');                                  /* Select the para element and store as "para" variable */
+    
+}
